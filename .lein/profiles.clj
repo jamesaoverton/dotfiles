84e711fe338ee9f9cd987427159c50100ff9d509
James A. Overton <james@overton.ca>
2020-02-06 21:09:46 -0500

    Update lein profile

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,6 +1,6 @@
 {:user
  {:signing {:gpg-key "9DBDED5B"}
-  :plugins [[cider/cider-nrepl "0.15.1"] ; used for vim-fireplace
+  :plugins [[cider/cider-nrepl "0.22.4"] ; used for vim-fireplace
             [lein-difftest "2.0.0"]      ; diff test output
-            [lein-cljfmt "0.5.6"]]       ; standardized code formatting
-  :dependencies [[cljfmt "0.5.6"]]}}
+            [lein-cljfmt "0.6.4"]]       ; standardized code formatting
+  :dependencies [[cljfmt "0.6.4"]]}}
