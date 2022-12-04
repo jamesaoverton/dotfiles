03e9afdf68cadb7c76f5eee679ae64f245a42e58
James A. Overton <james@overton.ca>
2022-12-04 21:04:48 +0000

    Add general nix/shell.nix

---
+++ nix/shell.nix
@@ -0,0 +1,35 @@
+with import <nixpkgs> {};
+
+stdenv.mkDerivation {
+  name = "home";
+  buildInputs = [
+    # terminal
+    mosh
+    starship
+    zellij
+
+    # editor
+    kakoune
+    helix
+    nodePackages.bash-language-server
+    ltex-ls
+    rnix-lsp
+    shellcheck
+    taplo
+    yaml-language-server
+
+    # utilities
+    gh
+    git
+    htop
+    jq
+    sqlite
+    tree
+    visidata
+  ];
+  shellHook = ''
+    zellij attach "$(hostname)" \
+    || zellij --session "$(hostname)" --layout "$(pwd)/.config/zellij/$(hostname).kdl" \
+    || zellij --session "$(hostname)" --layout "$(pwd)/.config/zellij/default.kdl"
+  '';
+}
