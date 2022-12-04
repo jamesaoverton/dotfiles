c3869a86f2e91a1beb530fa8bce5e5ad8ac24bc9
James A. Overton <james@overton.ca>
2022-12-04 18:14:36 +0000

    Move .config/* to top

--- .lein/profiles.clj
+++
@@ -1,6 +0,0 @@
-{:user
- {:signing {:gpg-key "9DBDED5B"}
-  :plugins [[cider/cider-nrepl "0.22.4"] ; used for vim-fireplace
-            [lein-difftest "2.0.0"]      ; diff test output
-            [lein-cljfmt "0.6.4"]]       ; standardized code formatting
-  :dependencies [[cljfmt "0.6.4"]]}}
